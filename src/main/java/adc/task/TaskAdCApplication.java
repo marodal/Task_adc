1a4f5d6646f0b8be25d6334bad9dd622ba25364b
marodal <marodal@gmail.com>
2025-04-11 11:58:43 +0200

    Update TaskAdCApplication.java

--- src/main/java/adc/task/TaskAdCApplication.java
+++ src/main/java/adc/task/TaskAdCApplication.java
@@ -26,18 +26,4 @@ public class TaskAdCApplication /*extends SpringBootServletInitializer*/{
 		SpringApplication.run(TaskAdCApplication.class, args);
 	}
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
-
 }
